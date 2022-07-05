c1b177aa7798d92457a1436490575df9d3399ac6
Timofei Panchenko <s7v7nthmusic@gmail.com>
2022-07-05 17:51:46 +0300

    Initial commit

---
+++ src/main/java/com/seventh/moneyfornothing/MoneyfornothingApplication.java
@@ -0,0 +1,13 @@
+package com.seventh.moneyfornothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyfornothingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyfornothingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seventh/moneyfornothing/MoneyfornothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seventh.moneyfornothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyfornothingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
